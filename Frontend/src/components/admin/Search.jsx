bc589cf790cc7767311d0e09b496613bde46b33e
Sameen K A <sameensameen60@gmail.com>
2024-06-05 12:29:21 +0530

    Search user

--- Frontend/src/components/admin/Search.jsx
+++ Frontend/src/components/admin/Search.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Cancel } from '../svg/svgIcons';
+import { searchUser } from '../../redux/admin/adminSlice';
+import { useDispatch } from "react-redux"
 import "../../assets/style/search.css";
 
 const Search = () => {
+   const [searchValue, setSearchValue] = useState("");
+   const dispatch = useDispatch();
+
+   const handleSearch = (event) => {
+      setSearchValue(event.target.value);
+      dispatch(searchUser(event.target.value.trim()));
+   }
+
+   const clearSearch = () => {
+      setSearchValue("");
+      dispatch(searchUser(""));
+   }
+
    return (
       <div className='search-container'>
          <div className='search-wrapper'>
-            <input type="text" className='search' placeholder='Search user' />
-            <div className='cancel-icon'>
-               <Cancel />
-            </div>
+            <input type="text" className='search' value={searchValue} onChange={(e) => handleSearch(e)} placeholder='Search user' />
+            {searchValue.length != 0 && (
+               <div className='cancel-icon' onClick={clearSearch}>
+                  <Cancel />
+               </div>
+            )}
          </div>
       </div>
    );
--- Frontend/src/redux/admin/adminSlice.js
+++ Frontend/src/redux/admin/adminSlice.js
@@ -6,23 +6,30 @@ const initialState = [];
 const adminSlice = createSlice({
   name: "adminSlice",
   initialState: {
+    orginalData: initialState,
     usersList: initialState,
   },
   reducers: {
-
+    searchUser: (state, action) => {
+      const searchName = action.payload.toLowerCase();
+      state.usersList = state.orginalData.filter((user) => user.name.toLowerCase().includes(searchName));
+    }
   },
   extraReducers: (builder) => {
     builder
       .addCase(fetchUser.fulfilled, (state, action) => {
         const userList = action.payload;
         state.usersList = userList;
+        state.orginalData = userList;
       })
       .addCase(deleteUser.fulfilled, (state, action) => {
         const deletedUserID = action.payload;
-        const afterDelete = state.usersList.filter((user) => user._id != deletedUserID && user);
-        state.usersList = afterDelete;
+        state.usersList = state.usersList.filter((user) => user._id != deletedUserID && user);;
+        state.orginalData = state.orginalData.filter((user) => user._id != deletedUserID && user);;
       })
   }
 });
 
+const { searchUser } = adminSlice.actions;
+export { searchUser }
 export default adminSlice.reducer;
