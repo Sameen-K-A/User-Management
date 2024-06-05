ebe4f63ba5ca0406841cea8eb9fbccecc348e7ea
Sameen K A <sameensameen60@gmail.com>
2024-06-05 22:59:36 +0530

    User registration

--- Frontend/src/components/user/Register.jsx
+++ Frontend/src/components/user/Register.jsx
@@ -1,22 +1,41 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import { registerUser } from "../../redux/user/userThunk";
+import { useNavigate } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
 import "../../assets/style/login.css";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegistration = async (event) => {
+    event.preventDefault();
+    const response = await registerUser({ name, email, phone, password, confirmPassword, toast });
+    if(response === "success"){
+      setTimeout(() => {
+        navigate('/');
+      }, 2500);
+    }
+  }
+
   return (
     <>
       <ToastContainer />
       <div className="container">
         <div className="login-center">
           <h1>Register</h1>
-          <form className="form">
-            <input type="text" placeholder="Name" className="input" autoComplete="name" />
-            <input type="text" placeholder="Email" className="input" autoComplete="email" />
-            <input type="text" placeholder="Phone number" className="input" autoComplete="tel" />
-            <input type="password" placeholder="Password" className="input" autoComplete="new-password" />
-            <input type="password" placeholder="Confirm Password" className="input" autoComplete="new-password" />
+          <form className="form" onSubmit={(e) => handleRegistration(e)}>
+            <input type="text" placeholder="Name" className="input" autoComplete="name" onChange={(e) => setName(e.target.value)} />
+            <input type="text" placeholder="Email" className="input" autoComplete="email" onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" placeholder="Phone number" className="input" autoComplete="tel" onChange={(e) => setPhone(e.target.value)} />
+            <input type="password" placeholder="Password" className="input" autoComplete="new-password" onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" placeholder="Confirm Password" className="input" autoComplete="new-password" onChange={(e) => setConfirmPassword(e.target.value)} />
             <button type="submit" className="button">Register</button>
             <p className="link">Already have an account <Link to={"/login"}>Login</Link></p>
           </form>
--- Frontend/src/redux/user/userThunk.js
+++ Frontend/src/redux/user/userThunk.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { localhostURL } from "../../services/url";
-import { createAsyncThunk } from "@reduxjs/toolkit"
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const loginVerification = createAsyncThunk(
   "userSlice/loginVerification",
@@ -26,3 +26,36 @@ export const loginVerification = createAsyncThunk(
     }
   }
 )
+
+export const registerUser = async ({ name, email, phone, password, confirmPassword, toast }) => {
+  name = name.trim();
+  email = email.trim();
+  phone = phone.trim();
+  password = password.trim();
+  confirmPassword = confirmPassword.trim();
+  const nameRegex = /^[a-zA-Z\s]{3,20}$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const phoneRegex = /^[6-9]\d{9}$/;
+
+  if (name === "" || email === "" || phone === "" || password === "" || confirmPassword === "") {
+    toast.warning("All the fields are required!", { hideProgressBar: true, autoClose: 3000 });
+  } else if (!nameRegex.test(name)) {
+    toast.warning("Name must be between 3 to 20 characters and contain only letters!", { hideProgressBar: true, autoClose: 3000 });
+  } else if (!emailRegex.test(email)) {
+    toast.warning("Invalid email address!", { hideProgressBar: true, autoClose: 3000 });
+  } else if (!phoneRegex.test(phone)) {
+    toast.warning("Invalid phone number!", { hideProgressBar: true, autoClose: 3000 });
+  } else if (password.length < 8) {
+    toast.warning("Password must be at least 8 characters long!", { hideProgressBar: true, autoClose: 3000 });
+  } else if (password !== confirmPassword) {
+    toast.warning("Password and confirm password do not match!", { hideProgressBar: true, autoClose: 3000 });
+  } else {
+    const response = await axios.post(`${localhostURL}/registerPost`, { name, email, phone, password })
+    if (response.data === "UserExist") {
+      toast.error("Email already exist", { hideProgressBar: true, autoClose: 3000 });
+    } else {
+      toast.success("Registration completed successfully", { hideProgressBar: true, autoClose: 2500 });
+      return "success";
+    }
+  }
+}
